6f4d1064e3589a5d5b2e9b4ef4e17709d4dc7646
Amy Venturino <aventuri@umich.edu>
2017-05-07 13:49:36 -0500

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,38 @@
+var express = require( 'express' );
+var app = express();
+var path = require( 'path' );
+var bodyParser = require( 'body-parser' );
+var mongoose = require( 'mongoose' );
+
+// global var
+var port = 8888;
+
+// mongoose
+mongoose.connect( 'mongodb://localhost:27017/wkndChal4');
+
+//schema
+var listingSchema = mongoose.Schema({
+  cost: Number,
+  rent: Number,
+  sqft: Number,
+  city: String
+}); // end schema
+
+// model
+var listing = mongoose.model( 'Listing', listingSchema );
+
+//uses
+app.use( bodyParser.urlencoded( { extended: true } ) );
+app.use( express.static( 'public' ) );
+
+
+//spin up server
+app.listen( port, function(){
+  console.log( 'server up on:', port );
+}); //end spin up server
+
+//base url
+app.get( '/', function( req, res ){
+  console.log( 'base url hit' );
+  res.sendFile( path.resolve( 'public/views/index.html' ) );
+}); //end base get
